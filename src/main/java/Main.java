a71a49d7b1ef386dd5270b8904d16df74a69809b
evgeny <tyztuz@gmail.com>
2020-06-10 12:53:30 +0500

    fixed put

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        Map<String, String> map = new MyHashMap();
+//        String a = map.put("1","1");
+//        String b = map.put("1","2");
+//
+//        System.out.println(b);
+        System.out.println(map.size());
+        System.out.println(map.containsKey("1"));
     }
 }
